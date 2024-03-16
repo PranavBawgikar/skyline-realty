3a94b8357f1f1f31695952b2489c06aff5cb7949
Pranav Bawgikar <102728016+PranavBawgikar@users.noreply.github.com>
2024-03-16 14:10:52 +0530

    Updated changes #3

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
+import './Contact.css'
+import { MdCall } from 'react-icons/md'
+import { BsFillChatDotsFill } from 'react-icons/bs'
+import { HiChatBubbleBottomCenter } from 'react-icons/hi' 
 
 const Contact = () => {
   return (
     <section className="c-wrapper">
         <div className="paddings innerWidth flexCenter c-container">
             {/* left side */}
-            <div className="c-left">
-                left
+            <div className="flexColStart c-left">
+                <span className="orangeText">Our contacts</span>
+                <span className="primaryText">Easy to contact us</span>
+                <span className="secondaryText">
+                    We are always ready to help by providing the best services for you. We believe a good place to live can make your life better.
+                </span>
+                <div className="flexColStart contactModes">
+                    {/* first row */}
+                    <div className="flexColStart row">
+                        <div className="flexColStart mode">
+                            <div className="flexStart">
+                                <div className="flexCenter icon">
+                                    <MdCall size={25} />
+                                </div>
+                                <div className="flexColStart detail">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
 
             {/* right side */}
